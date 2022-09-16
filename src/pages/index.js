f170355c8b4915a9a1617a91cf0e2b234f845a4f
Makkie-K <hortensia221974@yahoo.co.jp>
2022-09-16 11:44:10 +0900

    Hydration errors occuring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,56 @@
 import * as React from "react";
+import Head from "next/head";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
+// import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import ProTip from "/src/components/utils/ProTip";
 import Link from "/src/components/utils/Link";
 import Copyright from "/src/components/utils/Copyright";
+import Layout, { siteTitle } from "/src/components/layouts/layout";
+import utilStyles from "/src/styles/utils.module.css";
+import { getSortedPostsData } from "/src/lib/posts";
 
-export default function Index() {
+export default function Index({ allPostsData }) {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container maxWidth="sm">
+        <section className={utilStyles.headingMd}>
+          <p>Vegan & Vegetarian</p>
+        </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
+        <Box sx={{ my: 4 }}>
+          <Link href="/posts/first-post" color="secondary">
+            Go to first-post
+          </Link>
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
+    </Layout>
   );
+}
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
