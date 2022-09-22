e26a51466b9f6e16e27e9d6fbac1bafb968d6d4f
Makkie-K <hortensia221974@yahoo.co.jp>
2022-09-22 13:24:35 +0900

    add category button on AppBar

---
+++ src/pages/accomodation-index.js
@@ -0,0 +1,3 @@
+export default function AccomodationIndex() {
+  return <p>Accomodation-index</p>;
+}
---
+++ src/pages/eat-drink-index.js
@@ -0,0 +1,3 @@
+export default function EatDrinkIndex() {
+  return <p>Eat & Drink Index</p>;
+}
