91c4993e137c0eff21808a6d75768af9a837613d
Makkie-K <hortensia221974@yahoo.co.jp>
2022-09-23 00:52:00 +0900

    add posts-index

---
+++ src/components/homes/CategoriesCard.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+export default function CategoriesCard(props) {
+  // console.log(props.name);
+  return (
+    <Card sx={{ maxWidth: 634 }}>
+      <CardActionArea sx={{ position: "relative" }}>
+        <CardMedia
+          component="img"
+          height="422"
+          image={props.image}
+          alt={props.name}
+        />
+        <CardContent
+          sx={{
+            position: "absolute",
+            width: "80%",
+            backgroundColor: "#fff",
+            opacity: "0.8",
+            left: "46px",
+            bottom: "5%",
+            height: "25%",
+            textAlign: "center",
+          }}
+        >
+          <Typography gutterBottom variant="h5" component="div">
+            {props.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.text}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
