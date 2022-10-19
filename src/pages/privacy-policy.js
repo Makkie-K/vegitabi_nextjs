0049d26f863df89b16afc40d5a692db15c540687
Makkie-K <hortensia221974@yahoo.co.jp>
2022-10-19 14:56:29 +0900

    update footer

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -1,13 +1,19 @@
 import Container from "@mui/material/Container";
+import Layout from "/src/components/layouts/layout";
+import Box from "@mui/material/Box";
 
 export default function PrivacyPolicy() {
   return (
-    <Container>
-      <img
-        src="/images/homes/privacy-policy4.jpg"
-        width="100%"
-        sx={{ padding: 0 }}
-      ></img>
-    </Container>
+    <Layout>
+      <Container maxWidth="md" sx={{ marginTop: "30px" }}>
+        <Box sx={{ width: "100%", maxWidth: "768px", margin: "0 auto" }}>
+          <img
+            src="/images/homes/privacy-policy4.jpg"
+            width="100%"
+            sx={{ padding: 0 }}
+          ></img>
+        </Box>
+      </Container>
+    </Layout>
   );
 }
