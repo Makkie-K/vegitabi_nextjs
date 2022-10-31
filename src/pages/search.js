797e2fa5bf0088ec6dad13f19e4ec2f74c739ad1
Makkie-K <hortensia221974@yahoo.co.jp>
2022-10-31 15:32:22 +0900

    update search for complex keywords

--- src/pages/search.js
+++ src/pages/search.js
@@ -5,10 +5,34 @@ import { getSearchedPostsData } from "src/lib/posts";
 
 export default function Search({ searchedPostsData }) {
   const results = JSON.parse(searchedPostsData);
+
   // console.log(results);
   return (
     <div>
-      {results.map((result) => (
+      {results.length > 0 ? (
+        <div>
+          検索結果あり
+          {/* {results.map((result) => (
+            <div key={result.id}>
+              <div>{result.id}</div>
+              <div>{result.title}</div>
+              <div>{result.contents}</div>
+              <div>{result.date}</div>
+              <div>{result.category}</div>
+              <div>{result.area}</div>
+              <div>{result.address}</div>
+              <div>{result.map}</div>
+              <div>{result.telephone}</div>
+              <div>{result.url}</div>
+              <div>{result.businessHour}</div>
+              <div>{result.others}</div>
+            </div>
+          ))} */}
+        </div>
+      ) : (
+        <div>検索結果なし</div>
+      )}
+      {/* {results.map((result) => (
         <div key={result.id}>
           <div>{result.id}</div>
           <div>{result.title}</div>
@@ -23,15 +47,16 @@ export default function Search({ searchedPostsData }) {
           <div>{result.businessHour}</div>
           <div>{result.others}</div>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
 
-export async function getServerSideProps(context) {
+export function getServerSideProps(context) {
   const keyword = context.query.keyword;
   let searchedPostsData = getSearchedPostsData(keyword);
   searchedPostsData = JSON.stringify(searchedPostsData);
+  // console.log(searchedPostsData);
   // console.log(searchedPostsData);
   return {
     props: {
