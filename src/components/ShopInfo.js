49c7dee11ea4f8c041f790f58840d86792b09e23
Makiko Katsuta <makikokatsuta@makikonoair.home>
2022-11-13 15:03:36 +1100

    update Splider.js

--- src/components/ShopInfo.js
+++ src/components/ShopInfo.js
@@ -1,4 +1,4 @@
-import { GoogleMap } from "/src/components/GoogleMap";
+/** @jsxImportSource @emotion/react*/ import { GoogleMap } from "/src/components/GoogleMap";
 import ShopModal from "/src/components/ShopModal";
 import utilStyles from "/src/styles/utils.module.css";
 import * as React from "react";
@@ -48,25 +48,11 @@ export default function ShopInfo({
     createData("メニュー", <ShopModal fileCount={fileCount} id={id} />),
   ];
 
-  // const rows = [
-  //   createData("店名", title),
-  //   createData("住所", address),
-  //   // <GoogleMap map={map} />,
-  //   createData("地図", <GoogleMap map={map} />),
-  //   createData("電話番号", telephone),
-  //   createData("営業時間", businessHour),
-  //   createData(
-  //     "ウェブサイト",
-  //     <Link target="_blank" rel="noopener noreferrer" href={url}>
-  //       {url}
-  //     </Link>
-  //   ),
-  //   // createData("ウェブサイト", url),
-  //   createData("その他", others),
-  //   createData("メニュー", <ShopModal fileCount={fileCount} id={id} />),
-  // ];
   return (
-    <TableContainer component={Paper}>
+    <TableContainer
+      component={Paper}
+      style={{ background: "rgba(220,220,220, 0.3)" }}
+    >
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableBody>
           {rows.map((row) => (
@@ -83,43 +69,5 @@ export default function ShopInfo({
         </TableBody>
       </Table>
     </TableContainer>
-    //   <table className={utilStyles.table}>
-    //     <tr>
-    //       <td className={utilStyles.shopInfo}>店舗名</td>
-    //       <td>{title}</td>
-    //     </tr>
-    //     <tr>
-    //       <td className={utilStyles.shopInfo}>住所</td>
-    //       <td>{address}</td>
-    //     </tr>
-    //     <tr>
-    //       <td>地図</td>
-    //       <td>
-    //         <GoogleMap map={map} />
-    //       </td>
-    //     </tr>
-    //     <tr>
-    //       <td>電話番号</td>
-    //       <td>{telephone}</td>
-    //     </tr>
-    //     <tr>
-    //       <td>ウェブサイト</td>
-    //       <td>{url}</td>
-    //     </tr>
-    //     <tr>
-    //       <td>営業時間</td>
-    //       <td>{businessHour}</td>
-    //     </tr>
-    //     <tr>
-    //       <td>メニュー</td>
-    //       <td>
-    //         <ShopModal fileCount={fileCount} />
-    //       </td>
-    //     </tr>
-    //     <tr>
-    //       <td>その他</td>
-    //       <td>{others}</td>
-    //     </tr>
-    //   </table>
   );
 }
