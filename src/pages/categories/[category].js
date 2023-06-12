129a698d5de8c7784dab92562b8f5173eb533120
Makiko Katsuta <makikokatsuta@MakikonoMacBook-Air.local>
2023-06-13 00:28:43 +1000

    add pagination to categories

--- src/pages/categories/[category].js
+++ src/pages/categories/[category].js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { getAllCategories, getPostDataByCategory } from "/src/lib/posts";
 import utilStyles from "/src/styles/utils.module.css";
 import Link from "/src/components/utils/Link";
@@ -6,8 +7,22 @@ import Box from "@mui/material/Box";
 import Layout from "/src/components/layouts/layout";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
+import Pagination from "@mui/material/Pagination";
 
 export default function CategoryIndex({ postData }) {
+  const [page, setPage] = useState(1);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(2);
+  const displayNum = 2;
+
+  const handleChange = (e, index) => {
+    const start = (index - 1) * displayNum;
+    const end = (index - 1) * 2 + displayNum;
+    setStart(start);
+    setEnd(end);
+    setPage(index);
+  };
+  const pageCount = Math.ceil(postData.length / displayNum);
   return (
     <Layout>
       <Container maxWidth="md" sx={{ marginTop: "90px}" }}>
@@ -21,84 +36,104 @@ export default function CategoryIndex({ postData }) {
           {postData[0].category}の記事一覧
         </Box>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          {postData.map(({ id, date, title, contents }) => (
-            <Box
-              className={utilStyles.listItem}
-              key={id}
-              sx={{
-                display: "flex",
-                flexDirection: { xs: "column", md: "row" },
-                border: "solid 1px lightgrey",
-                padding: "5px",
-              }}
-            >
-              <Box sx={{ width: { xs: "100%", md: "50%" } }}>
-                <Link href={`/posts/${id}`}>
-                  <Avatar
-                    alt={title}
-                    src={`/images/posts/${id}/1.webp`}
-                    variant="square"
-                    // sx={{ width: "100%", height: 135 }}
-                    sx={{ width: "100%", height: 180 }}
-                  />
-                </Link>
-              </Box>
+          {postData
+            .slice(start, end)
+            // .slice(start, postData.length - (page - 1) * 2)
+            .map(({ id, date, title, contents, index }) => (
+              // {postData.map(({ id, date, title, contents }) => (
               <Box
+                className={utilStyles.listItem}
+                key={id}
                 sx={{
-                  paddingLeft: "5px",
-                  width: { xs: "100%", md: "50%" },
                   display: "flex",
-                  justifyContent: "space-between",
-                  flexDirection: "column",
+                  flexDirection: { xs: "column", md: "row" },
+                  border: "solid 1px lightgrey",
+                  padding: "5px",
                 }}
               >
-                <Box sx={{ margin: "9px" }}>
-                  <Link
-                    href={`/posts/${id}`}
-                    sx={{
-                      display: "-webkitBox",
-                      textDecoration: "none",
-                      color: "rgba(0, 0, 0, 0.55)",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      WebkitBoxOrient: "vertical",
-                      WebkitLineClamp: "2",
-                    }}
-                  >
-                    {title}
+                <Box sx={{ width: { xs: "100%", md: "50%" } }}>
+                  <Link href={`/posts/${id}`}>
+                    <Avatar
+                      alt={title}
+                      src={`/images/posts/${id}/1.webp`}
+                      variant="square"
+                      // sx={{ width: "100%", height: 135 }}
+                      sx={{ width: "100%", height: 180 }}
+                    />
                   </Link>
-                  <small className={utilStyles.lightText}>
-                    <Date dateString={date} />
-                  </small>
                 </Box>
                 <Box
                   sx={{
-                    margin: "9px",
-                    height: "60px",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    // display: "-webkit-box",
-                    display: "-webkitBox",
-                    // "-webkitBoxOrient": "vertical",
-                    // "-webkitLineClamp": "2",
+                    paddingLeft: "5px",
+                    width: { xs: "100%", md: "50%" },
+                    display: "flex",
+                    justifyContent: "space-between",
+                    flexDirection: "column",
                   }}
                 >
-                  <Link
-                    href={`/posts/${id}`}
+                  <Box sx={{ margin: "9px" }}>
+                    <Link
+                      href={`/posts/${id}`}
+                      sx={{
+                        display: "-webkitBox",
+                        textDecoration: "none",
+                        color: "rgba(0, 0, 0, 0.55)",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        WebkitBoxOrient: "vertical",
+                        WebkitLineClamp: "2",
+                      }}
+                    >
+                      {title}
+                    </Link>
+                    <small className={utilStyles.lightText}>
+                      <Date dateString={date} />
+                    </small>
+                  </Box>
+                  <Box
                     sx={{
-                      color: "rgba(0, 0, 0, 0.55)",
-                      textDecoration: "none",
-                      fontSize: "85%",
+                      margin: "9px",
+                      height: "60px",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      // display: "-webkit-box",
+                      display: "-webkitBox",
+                      // "-webkitBoxOrient": "vertical",
+                      // "-webkitLineClamp": "2",
                     }}
                   >
-                    {contents}
-                  </Link>
+                    <Link
+                      href={`/posts/${id}`}
+                      sx={{
+                        color: "rgba(0, 0, 0, 0.55)",
+                        textDecoration: "none",
+                        fontSize: "85%",
+                      }}
+                    >
+                      {contents}
+                    </Link>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-          ))}
+            ))}
           {/* </ul> */}
         </section>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "30px",
+          }}
+        >
+          <Box>
+            <Pagination
+              // count={Math.ceil(postData.length / 2)}
+              count={pageCount}
+              page={page}
+              onChange={handleChange}
+            />
+          </Box>
+        </Box>
       </Container>
     </Layout>
   );
