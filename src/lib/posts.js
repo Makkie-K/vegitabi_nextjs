b696606d4f2a21f44a256ecd4925b42d705c83d0
makiko <hortensia221974@yahoo.co.jp>
2024-05-17 15:02:43 +1000

    update search function10

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -187,27 +187,56 @@ export function getPostDataByArea(area) {
 }
 
 // keywordを含んだ記事をreturnする
+// export function getSearchedPostsData(keyword) {
+//   // console.log(keyword);
+//   const keywords = handleKeyword(keyword);
+//   let results = [];
+//   let hitIDs = [];
+
+//   if (keywords.length !== 0) {
+//     const allPostsData = getSortedPostsData();
+//     // console.log(allPostsData);
+
+//     // title
+//     // titlejp
+//     // category
+//     // categoryJp
+//     // area
+//     // areaJp
+//     // address
+//     // others
+//     // 検索結果を格納する配列
+//     const hitObjects = [];
+//     // AND検索処理
+//     const lowercasedKeywords = keywords.map((keyword) => keyword.toLowerCase());
+
+//     allPostsData.forEach((obj) => {
+//       const isMatch = lowercasedKeywords.every(
+//         (keyword) =>
+//           obj.title.toLowerCase().includes(keyword) ||
+//           obj.titlejp.includes(keyword) ||
+//           obj.category.toLowerCase().includes(keyword) ||
+//           obj.categoryJp.includes(keyword) ||
+//           obj.area.toLowerCase().includes(keyword) ||
+//           obj.areaJp.includes(keyword) ||
+//           obj.address.includes(keyword) ||
+//           obj.others.includes(keyword)
+//       );
+//       if (isMatch) {
+//         hitObjects.push(obj);
+//       }
+//     });
+//     // console.log(hitObjects);
+//     return hitObjects;
+//   }
+//   return [];
+// }
 export function getSearchedPostsData(keyword) {
-  // console.log(keyword);
-  const keywords = handleKeyword(keyword);
-  let results = [];
-  let hitIDs = [];
+  const keywords = keyword ? handleKeyword(keyword) : [];
+  const hitObjects = [];
 
   if (keywords.length !== 0) {
     const allPostsData = getSortedPostsData();
-    // console.log(allPostsData);
-
-    // title
-    // titlejp
-    // category
-    // categoryJp
-    // area
-    // areaJp
-    // address
-    // others
-    // 検索結果を格納する配列
-    const hitObjects = [];
-    // AND検索処理
     const lowercasedKeywords = keywords.map((keyword) => keyword.toLowerCase());
 
     allPostsData.forEach((obj) => {
@@ -226,10 +255,8 @@ export function getSearchedPostsData(keyword) {
         hitObjects.push(obj);
       }
     });
-    // console.log(hitObjects);
-    return hitObjects;
   }
-  return [];
+  return hitObjects;
 }
 
 export function handleKeyword(keyword) {
