8f013fe7da141a2d125d496d2c775bc8d0f0ee5e
makiko <hortensia221974@yahoo.co.jp>
2024-05-18 15:44:30 +1000

    update search function18

--- src/pages/search.js
+++ src/pages/search.js
@@ -30,58 +30,8 @@ export default function Search({ searchedPostsData, keyword }) {
           {results.length > 0 ? (
             <div>
               {results.map((result) => (
-                <Box
-                  className={utilStyles.listItem}
-                  key={result.id}
-                  sx={{
-                    display: "flex",
-                    flexDirection: { xs: "column", md: "row" },
-                    border: "solid 1px lightgrey",
-                    padding: "5px",
-                  }}
-                >
-                  <Box sx={{ width: { xs: "100%", md: "50%" } }}>
-                    <Link href={`/posts/${result.id}`}>
-                      <Avatar
-                        alt={result.title}
-                        src={`/images/posts/${result.id}/1.webp`}
-                        variant="square"
-                        // sx={{ width: "100%", height: 135 }}
-                        sx={{ width: "100%", height: 180 }}
-                      />
-                    </Link>
-                  </Box>
-                  <Box
-                    sx={{
-                      paddingLeft: "5px",
-                      width: { xs: "100%", md: "50%" },
-                      display: "flex",
-                      justifyContent: "space-between",
-                      flexDirection: "column",
-                    }}
-                  >
-                    <Box sx={{ margin: "9px" }}>
-                      <Link
-                        href={`/posts/${result.id}`}
-                        sx={{
-                          // display: "flex",
-                          //
-                          display: "-webkitBox",
-                          textDecoration: "none",
-                          color: "rgba(0, 0, 0, 0.55)",
-                          overflow: "hidden",
-                          textOverflow: "ellipsis",
-                          "-webkitBoxOrient": "vertical",
-                          "-webkitLineClamp": "2",
-                        }}
-                      >
-                        {result.title}
-                      </Link>
-                      <small className={utilStyles.lightText}>
-                        <Date dateString={result.date} />
-                      </small>
-                    </Box>
-                  </Box>
+                <Box sx={{ margin: "9px" }} key={result.id}>
+                  {result.title}
                 </Box>
               ))}
             </div>
@@ -93,6 +43,89 @@ export default function Search({ searchedPostsData, keyword }) {
     </Layout>
   );
 }
+// export default function Search({ searchedPostsData, keyword }) {
+//   // export default function Search({ keyword }) {
+//   const results = JSON.parse(searchedPostsData);
+//   console.log(results);
+//   return (
+//     <Layout>
+//       <Container maxWidth="md" sx={{ marginTop: "30px}" }}>
+//         <Box
+//           sx={{
+//             textAlign: "center",
+//             backgroundColor: "lightgrey",
+//             marginBottom: "30px",
+//           }}
+//         >
+//           「<span>{keyword}</span>」を含む記事一覧
+//         </Box>
+//         <section>
+//           {results.length > 0 ? (
+//             <div>
+//               {results.map((result) => (
+//                 <Box
+//                   className={utilStyles.listItem}
+//                   key={result.id}
+//                   sx={{
+//                     display: "flex",
+//                     flexDirection: { xs: "column", md: "row" },
+//                     border: "solid 1px lightgrey",
+//                     padding: "5px",
+//                   }}
+//                 >
+//                   <Box sx={{ width: { xs: "100%", md: "50%" } }}>
+//                     <Link href={`/posts/${result.id}`}>
+//                       <Avatar
+//                         alt={result.title}
+//                         src={`/images/posts/${result.id}/1.webp`}
+//                         variant="square"
+//                         // sx={{ width: "100%", height: 135 }}
+//                         sx={{ width: "100%", height: 180 }}
+//                       />
+//                     </Link>
+//                   </Box>
+//                   <Box
+//                     sx={{
+//                       paddingLeft: "5px",
+//                       width: { xs: "100%", md: "50%" },
+//                       display: "flex",
+//                       justifyContent: "space-between",
+//                       flexDirection: "column",
+//                     }}
+//                   >
+//                     <Box sx={{ margin: "9px" }}>
+//                       <Link
+//                         href={`/posts/${result.id}`}
+//                         sx={{
+//                           // display: "flex",
+//                           //
+//                           display: "-webkitBox",
+//                           textDecoration: "none",
+//                           color: "rgba(0, 0, 0, 0.55)",
+//                           overflow: "hidden",
+//                           textOverflow: "ellipsis",
+//                           "-webkitBoxOrient": "vertical",
+//                           "-webkitLineClamp": "2",
+//                         }}
+//                       >
+//                         {result.title}
+//                       </Link>
+//                       <small className={utilStyles.lightText}>
+//                         <Date dateString={result.date} />
+//                       </small>
+//                     </Box>
+//                   </Box>
+//                 </Box>
+//               ))}
+//             </div>
+//           ) : (
+//             <div>検索結果なし</div>
+//           )}
+//         </section>
+//       </Container>
+//     </Layout>
+//   );
+// }
 
 export async function getServerSideProps(context) {
   const keyword = context.query.keyword;
