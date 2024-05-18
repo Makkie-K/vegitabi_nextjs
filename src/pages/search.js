0f7274fabf43bda5a48e5db8a11e1e294f5fe692
makiko <hortensia221974@yahoo.co.jp>
2024-05-18 17:23:06 +1000

    update search function20

--- src/pages/search.js
+++ src/pages/search.js
@@ -3,11 +3,14 @@ import Layout from "/src/components/layouts/layout";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 
-export default function Search({ keyword }) {
+export default function Search({ keyword, results }) {
   return (
     <Layout>
       <Container>
         <Box sx={{ marginTop: "100px" }}>{keyword}</Box>
+        {results.map((result) => (
+          <Box key={result.id}>{result.title}</Box>
+        ))}
       </Container>
     </Layout>
   );
@@ -16,5 +19,12 @@ export default function Search({ keyword }) {
 export async function getServerSideProps(context) {
   console.log(context.query.keyword);
   const keyword = context.query.keyword;
-  return { props: { keyword } };
+  const results = [
+    { id: 1, title: "りんご" },
+    { id: 2, title: "みかん" },
+    { id: 3, title: "バナナ" },
+    { id: 4, title: "メロン" },
+    { id: 5, title: "苺" },
+  ];
+  return { props: { keyword, results } };
 }
