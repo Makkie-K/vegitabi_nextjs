fe942e333cb02f71e8062e415fbb2806ac06bccc
makiko <hortensia221974@yahoo.co.jp>
2024-05-19 13:30:53 +1000

    update search function23

--- src/lib/getResultsBySearch.js
+++ src/lib/getResultsBySearch.js
@@ -1,18 +1,10 @@
-import { getSortedPostsData } from "/src/lib/posts";
-
 export const getResultsBySearch = () => {
-  const sortedPostsData = getSortedPostsData();
-  console.log(sortedPostsData);
-  const results = sortedPostsData.map((post) => ({
-    id: post.id,
-    title: post.title,
-  }));
-  // const results = [
-  //   { id: 1, title: "りんご" },
-  //   { id: 2, title: "みかん" },
-  //   { id: 3, title: "バナナ" },
-  //   { id: 4, title: "メロン" },
-  //   { id: 5, title: "苺" },
-  // ];
+  const results = [
+    { id: 1, title: "りんご" },
+    { id: 2, title: "みかん" },
+    { id: 3, title: "バナナ" },
+    { id: 4, title: "メロン" },
+    { id: 5, title: "苺" },
+  ];
   return results;
 };
