37311d8eb0fbfb82062767179c6eb2fd47d98e9a
makiko <hortensia221974@yahoo.co.jp>
2024-05-21 12:48:17 +1000

    update ANDsearch1

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -19,11 +19,15 @@ import Typography from "@mui/material/Typography";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 export async function getServerSideProps({ query }) {
-  const keyword = query.keyword ? query.keyword.toLowerCase() : "";
+  const keyword = query.keyword ? query.keyword.trim().toLowerCase() : "";
 
   if (!keyword) {
     return { props: { filteredPostsData: [], keyword } };
   }
+
+  // キーワードをスペースで分割して配列にする
+  const keywordsArray = keyword.split(/[\s,、。;；\n]+/);
+  console.log(keywordsArray);
   let apiUrl;
   if (process.env.NODE_ENV === "production") {
     apiUrl = process.env.NEXT_PUBLIC_API_URL_PROD;
@@ -34,16 +38,17 @@ export async function getServerSideProps({ query }) {
     const res = await fetch(apiUrl);
     const allPostsData = await res.json();
 
-    // キーワードに基づいてデータをフィルタリング
-    const filteredPostsData = allPostsData.filter(
-      (postData) =>
-        postData.title.includes(keyword) ||
-        postData.titlejp.includes(keyword) ||
-        postData.category.includes(keyword) ||
-        postData.categoryJp.includes(keyword) ||
-        postData.area.includes(keyword) ||
-        postData.areaJp.includes(keyword) ||
-        postData.others.includes(keyword)
+    const filteredPostsData = allPostsData.filter((postData) =>
+      keywordsArray.every(
+        (key) =>
+          postData.title.includes(key) ||
+          postData.titlejp.includes(key) ||
+          postData.category.includes(key) ||
+          postData.categoryJp.includes(key) ||
+          postData.area.includes(key) ||
+          postData.areaJp.includes(key) ||
+          postData.others.includes(key)
+      )
     );
 
     return { props: { filteredPostsData, keyword } };
@@ -65,9 +70,7 @@ export default function Search({ filteredPostsData, keyword }) {
             backgroundColor: "",
             marginBottom: "30px",
           }}
-        >
-          検索結果
-        </Box>
+        ></Box>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <Grid container spacing={2}>
             {results.length > 0 ? (
@@ -108,64 +111,10 @@ export default function Search({ filteredPostsData, keyword }) {
                       />
                       <CardContent>
                         <Typography variant="body2" color="text.secondary">
-                          最終更新日: <Date dateString={result.date} />{" "}
-                          {/* 日付を表示 */}
+                          <Date dateString={result.date} /> {/* 日付を表示 */}
                         </Typography>
                       </CardContent>
                     </Card>
-
-                    {/* <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        height: "100%",
-                        borderRadius: "4px",
-                        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-                        transition: "transform 0.3s ease-in-out",
-                        "&:hover": {
-                          transform: "translateY(-5px)",
-                        },
-                      }}
-                    >
-                      <Avatar
-                        alt={result.title}
-                        src={`/images/posts/${result.id}/${result.avator}`}
-                        sx={{
-                          width: "100%",
-                          height: 200,
-                          objectFit: "cover",
-                          borderTopLeftRadius: "4px",
-                          borderTopRightRadius: "4px",
-                        }}
-                      />
-                      <Box p={2} flexGrow={1}>
-                        <Box
-                          sx={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                            marginBottom: "8px",
-                          }}
-                        >
-                          <Box
-                            sx={{
-                              fontSize: "1.1rem",
-                              fontWeight: "bold",
-                            }}
-                          >
-                            {result.title}
-                          </Box>
-                          <Box
-                            sx={{
-                              fontSize: "0.9rem",
-                              color: "rgba(0, 0, 0, 0.5)",
-                            }}
-                          >
-                            {result.date}
-                          </Box>
-                        </Box>
-                      </Box>
-                    </Box> */}
                   </Link>
                 </Grid>
               ))
