146586ef8dc5e47d3d856392777337ec4306c71a
makiko <hortensia221974@yahoo.co.jp>
2024-06-17 01:05:46 +1000

    fix git-error3

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  exportPathMap: async function () {
-    return {
-      // 除外したい動的ページのパスを指定
-      "/search": { page: "/search" }, // 例: /search ページを /404 にマップ
-    };
-  },
-  images: {
-    unoptimized: true,
-  },
   reactStrictMode: true,
 };
