c2c5b3032750356b8cfe148307bee82af6507ddc
makiko <hortensia221974@yahoo.co.jp>
2024-06-16 14:34:35 +1000

    add image2home

--- out/_next/static/YGluq1EksKQpBPgBoVUIE/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fareas\u002F[area]","\u002Fcategories\u002F[category]","\u002Fcolumns","\u002Fcolumns\u002F[id]","\u002Fposts","\u002Fposts\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Container from "@mui/material/Container";
 // import Typography from "@mui/material/Typography";
@@ -12,9 +12,49 @@ import { getSortedPostsData } from "/src/lib/posts";
 import Date from "/src/components/date";
 import Categories from "/src/components/homes/Categories";
 import Areas from "/src/components/homes/Areas";
+import AppBar from "@mui/material/AppBar";
+import { useRouter } from "next/router";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+
 // import Header from "/src/components/layouts/Header";
 
 export default function Index() {
+  const [open, setOpen] = useState(false);
+  const [keyword, setKeyword] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.keyword) {
+      setKeyword(router.query.keyword);
+    }
+  }, [router.query.keyword]);
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  const handleOpenMenu = (e) => {
+    setOpen(!open);
+  };
+
+  const handleCloseMenu = (e) => {
+    setOpen(false);
+  };
+
+  const handleSubmit = (e) => {
+    // enterを押したときに改行するのを防ぐため
+    e.preventDefault();
+    if (keyword.trim() === "") {
+      // キーワードが空の場合、アラートを表示するか、単に何もしない
+      alert("検索キーワードを入力してください");
+      return;
+    }
+    router.push({
+      pathname: "/search",
+      query: { keyword: keyword },
+    });
+    handleCloseMenu();
+  };
   return (
     <Layout>
       {/* <Header /> */}
@@ -42,6 +82,27 @@ export default function Index() {
         />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
+      <AppBar position="relative">
+        <Box
+          component="img"
+          sx={{ width: "100%" }}
+          alt="Logo"
+          src="/images/homes/top-search.webp" // ここに画像のパスを指定します
+        />
+        <Box component="form" onSubmit={handleSubmit}>
+          <TextField
+            id="outlined-basic"
+            placeholder="検索"
+            variant="outlined"
+            size="normal"
+            fullWidth
+            InputProps={{ endAdornment: <SearchIcon /> }}
+            onChange={handleChange}
+            value={keyword}
+          />
+        </Box>
+      </AppBar>
+
       <Container sx={{ paddingTop: "100px" }}>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <Categories />
